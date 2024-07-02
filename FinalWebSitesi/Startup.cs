d26142e193bf2a3b8e47f6a1d0128131d3c90476
Baris <Baris@DESKTOP-SSTNNIP>
2024-07-02 11:41:45 +0300

    Proje dosyası ekle.

---
+++ FinalWebSitesi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalWebSitesi.Startup))]
+namespace FinalWebSitesi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
